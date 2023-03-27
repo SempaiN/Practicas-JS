0a378d0adda77ff686149aad9524ff5f8969d8f8
pokB01 <pokB01@x.y.z>
2023-03-27 13:05:55 +0200

    hasta el 8

--- 4.1/Practica4.1.js
+++ 4.1/Practica4.1.js
@@ -27,6 +27,7 @@ switch (elec) {
         else {
             document.write("El número es negativo")
         }
+        document.write(" ")
         if ((n1 % 2) == 0) {
             document.write("El númeor es par")
         }
@@ -37,13 +38,68 @@ switch (elec) {
 
     case "3":
         var suma = 0;
-        for (let index = 0; index <= 100; index++) {
+        for (let index = 1; index <= 100; index++) {
             suma+= index;
         }
         document.write(suma);
-        
         break;
-    default:
+    
+    case "4":
+        var empieza = parseInt(prompt("Introduce el número"));
+        var suma = 0;
+        for (let index = empieza; index < empieza+100;index ++){
+            suma+=  index;
+        }
+        document.write(suma)
+        break;
+
+    case "5":
+        n1 = prompt("Dime el número 1")
+        n2 = prompt("Dime el número 2")
+        n3 = prompt("Dime el número 3")
+        var nums = [n1,n2,n3];
+        nums.sort;
+        document.write("El numero más grande es el " + nums[2] + " El número más pequeño es el " + nums[0])
+        break;
+    case "6":
+        var nums = [];
+        var num;
+        var cont = 0;
+        do {
+            num = prompt("Dame un número");
+            if (num <= 0 ) {
+                break;
+            }
+            nums[cont] = num;
+            cont++;
+            
+        } while (num > 0);
+        nums.sort;
+        var largo = nums.length;
+        document.write("El número mayor es " + nums[largo-1] + " El número menor es el " + nums[0])
+        break;
+    case "7":
+        var cont = 0;
+        var temperaturas = [];
+        do {
+            var temperatura = parseInt(prompt("Dame la temperatura en Farenheit"));
+            if (temperatura == 999) {
+                break;
+            }
+            temperaturas[cont] = (temperatura-32) * 5/9;
+            document.write(temperaturas[cont] + " , ")
+            cont ++;
+
+        } while (temperatura != 999);
+        break;
+
+    case "8":
+        var kmh = parseInt(prompt("Dame la temperatura en km/h"));
+        var ms = kmh/3.6;
+        var distancia = parseInt(prompt("Dame la distancia"))
+        document.write("Haces  " + + distancia*ms + " metros")
+        break;
+        default:
 
         break;
 }
