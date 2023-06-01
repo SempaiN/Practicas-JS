2491e99ebe376b33890b0ae81e3bee8fe81eebc5
SempaiN <trollnacho.np@gmail.com>
2023-06-01 12:55:19 +0200

    kekekekkee

---
+++ 4.5/4.5.js
@@ -0,0 +1,43 @@
+// setTimeout(mostrar,3000);
+// function mostrar(){
+//     document.write("Han pasado 3 segundo")
+// }
+
+// setTimeout(function(){mostrar("Han pasado 3 segundos");},3000);
+// function mostrar(value){
+//     document.write(value)
+// }
+
+// let miPromesa = new Promise(function(siExito,siFracaso){
+//     siExito();
+//     siFracaso();
+// });
+
+// miPromesa.then(
+
+//     function(value){
+
+//     },
+//     function(error){
+
+//     }
+// );
+
+// function myDisplayer(some) {
+//     document.getElementById("demo").innerHTML = some;
+// }
+  
+// let myPromise = new Promise(function(myResolve, myReject) {
+//     let x = 9;
+  
+//   // some code (try to change x to 5)
+  
+//     if (x == 0) {
+//       myResolve("OK");
+//     } else {
+//       myReject("Error");
+//     }
+//   });
+  
+
+
